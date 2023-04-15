44b8a84a2efb0fa3bc993fe847ab7a1153a7819d
Kate Velichko <bboobb0503@gmail.com>
2023-04-15 22:41:15 +0300

    third commit

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 const express = require('express');
+
 const mongoose = require('mongoose');
 
 const bodyParser = require('body-parser');
 
- const routeUsers = require('./routes/users');
- const routeCards = require('./routes/cards');
+const routeUsers = require('./routes/users');
+
+const routeCards = require('./routes/cards');
 
 const { PORT = 3000 } = process.env;
 
 mongoose.set('strictQuery', true);
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb')
-.then(() => console.log('Успешное подключение к MongoDB'))
-.catch((err) => console.error('Ошибка подключения:', err));
+  .then(() => console.log('Успешное подключение к MongoDB'))
+  .catch((err) => console.error('Ошибка подключения:', err));
 
 const app = express();
-
-
 app.use((req, res, next) => {
   req.user = {
-    _id: '643aacdc1a4bc372d5b763b9'
+    _id: '643aacdc1a4bc372d5b763b9',
   };
 
   next();
@@ -33,4 +33,4 @@ app.use('/cards', routeCards);
 
 app.use((req, res, next) => next(console.log('ошибка')));
 
-app.listen(PORT, () => {console.log('я подключился')});
+app.listen(PORT, () => { console.log('я подключился'); });
