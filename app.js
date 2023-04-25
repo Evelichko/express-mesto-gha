c1aad6e4f99ca5c6fd1375e2a1aad7dfdb3b1af1
Kate Velichko <bboobb0503@gmail.com>
2023-04-25 17:40:43 +0300

    25 04 3c

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const { celebrate, Joi } = require('celebrate');
 const { errors } = require('celebrate');
 const bodyParser = require('body-parser');
 
 const routeUsers = require('./routes/users');
 const routeCards = require('./routes/cards');
+const routeSignUp = require('./routes/signUp');
+const routeSignIn = require('./routes/signIn');
 
 const { auth } = require('./middlewares/auth');
 const { handleError } = require('./middlewares/handleError');
@@ -17,32 +18,13 @@ const NotFoundError = require('./errors/NotFoundError');
 mongoose.set('strictQuery', true);
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
-const login = require('./controllers/users');
-const createUser = require('./controllers/users');
-
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required().min(6),
-  }),
-}), login);
-
-app.post('/signup', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required().min(6),
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    avatar: Joi
-      .string()
-      .pattern(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/),
-  }),
-}), createUser);
+app.use('/', routeSignUp);
+app.use('/', routeSignIn);
 
 app.use(auth);
 
