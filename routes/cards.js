acb03fc0f24b176616bbef2a779b4dc2a4ed3bd3
Kate Velichko <bboobb0503@gmail.com>
2023-04-26 08:24:44 +0300

    26 04 2c

--- routes/cards.js
+++ routes/cards.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 
 const {
   createCard,
@@ -8,10 +9,29 @@ const {
   likeCard,
 } = require('../controllers/cards');
 
-router.post('/', createCard);
+router.post('/', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string()
+      .required()
+      .pattern(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/),
+  }),
+}), createCard);
+
 router.get('/', getCards);
-router.delete('/:id', deleteCard);
-router.put('/:cardId/likes', likeCard);
+
+router.delete('/:id', celebrate({
+  params: Joi.object().keys({
+    id: Joi.string().length(24).hex().required(),
+  }),
+}), deleteCard);
+
+router.put('/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().length(24).hex().required(),
+  }),
+}), likeCard);
+
 router.delete('/:cardId/likes', dislikeCard);
 
 module.exports = router;
