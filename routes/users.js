0e67bc7515f972b1a3882b619c8240040786e6a1
Kate Velichko <bboobb0503@gmail.com>
2023-05-01 17:27:37 +0300

    1 05 6c

--- routes/users.js
+++ routes/users.js
@@ -21,8 +21,8 @@ router.get('/:id', celebrate({
 
 router.patch('/me', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
+    name: Joi.string().min(2).max(30).required(),
+    about: Joi.string().min(2).max(30).required(),
   }),
 }), setUserInfo);
 
@@ -30,7 +30,8 @@ router.patch('/me/avatar', celebrate({
   body: Joi.object().keys({
     avatar: Joi
       .string()
-      .pattern(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/),
+      .pattern(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/)
+      .required(),
   }),
 }), setUserAvatar);
 
